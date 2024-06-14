833b76e727d64e29ca90eb58c5c86adc53424849
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-06-14 16:10:30 -0500

    add comments for key highlights

--- etch.js
+++ etch.js
@@ -1,6 +1,26 @@
 const COLUMNS =16;
 const ROWS = 16;
-
+/**
+ * 1. 16x16 Grid with styling
+ * 2. Hover effect to change color when mouse hovers over grid
+ * 3. Prompt for a new etch pad 
+ * 4. 
+ */
 function create_divs(){
-    
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.appendChild(".container");
+
+    for(let i =0;i<COLUMNS;i++){
+        const div = document.createElement("div");
+        div.classList.add("grid");
+        div.appendChild(".container");
+        
+        console.log(i+ "row");
+
+        for(let j=0;j<ROWS;j++){
+            
+
+        }
+    }
 }
