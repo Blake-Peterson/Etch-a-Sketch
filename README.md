3f7f213e37150789c165c96f77f5d919c737c42c
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-07-16 16:14:53 -0500

    got rows and boxes joined together, and update readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Etch-a-Sketch project from The Odin Project.
 
 1. Creating a 16x16 Grid of square divs using JS
 2. Setup a Hovering when the mouse enters a div and leaves a div
-3. Add a button on top of the screen that will send the user a popup for a new grid to be created based on the size selected by them, removing the old grid
+3. Add a button on top of the screen that will send the user a popup for a new grid to be created based on the size selected by them, removing the old grid within the same space of 960px
--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 const container = document.querySelector("#container")
 
-function createGrid(){
+function createGrid(row_size, box_size){
 
-    for(let i=0;i<16;i++){
+    for(let i=0;i<row_size;i++){
 
         const row = document.createElement("div");
+        row.style.height = "30px";
+        row.setAttribute('id','row${j}');
         row.classList.add("row");
-
-
         row.textContent="Rows";
         container.appendChild(row);
 
-        for(let j=0;j<16;j++){
-            const box = document.querySelector("div");
-            box.classList.add("box");
-
-
-            box.appendChild(row);
-
-        }
+        for(let j=0;j<box_size;j++){
+            const boxes = document.createElement("div"); 
+            boxes.style.width= "30px";
+            boxes.setAttribute('id','cells${j}');
+            boxes.classList.add("boxes");
+            boxes.textContent="boxes";
+            row.appendChild(boxes);
+            
+        } 
     }
+}
+createGrid(16,16);
+
+function change_grid_color(){
+    
+}
+
+function clear_grid(){
 
 }
 
-createGrid();
+function update_grid_size(){
+
+}
+
+function get_new_grid_size(){
+    let new_size = prompt("How many Grids would you like? (100 or less)");
+    if(new_size > 100){
+        new_size = prompt("You chose a number too big, please try again");
+    }
+    return new_size;
+}
